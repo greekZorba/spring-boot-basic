d138d563ea3056916e31b07e1edfc7d06eeb41eb
greekZorba <smiling.jinhak@gmail.com>
2020-07-07 08:53:02 +0900

    자동 설정(auto configuration) 이해

--- src/main/java/com/zorba/springbootbasic/SpringBootBasicApplication.java
+++ src/main/java/com/zorba/springbootbasic/SpringBootBasicApplication.java
@@ -3,6 +3,14 @@ package com.zorba.springbootbasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * ComponentScan 애노테이션으로 빈 읽어들이고 등록한 다음,
+ * EnableAutoConfiguration 애노테이션으로 추가적으로 읽어들인 빈을 다시 등록한다.
+ *
+ * EnableAutoConfiguration
+ * spring.factories에 있는 키값을 통해 빈들을 등록한다.
+ * 빈들도 조건에 따라 생성될지 말지 결정됨
+ * */
 @SpringBootApplication
 public class SpringBootBasicApplication {
 
