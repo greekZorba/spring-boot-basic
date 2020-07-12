f345140c658fda10916d6c0bae1b059d360f47d8
greekZorba <smiling.jinhak@gmail.com>
2020-07-12 23:47:37 +0900

    내장 웹 서버 응용 1부 : 컨테이너와 포트

---
+++ src/main/java/com/zorba/springbootbasic/PortListener.java
@@ -0,0 +1,16 @@
+package com.zorba.springbootbasic;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent> {
+
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent servletWebServerInitializedEvent) {
+        ServletWebServerApplicationContext applicationContext = servletWebServerInitializedEvent.getApplicationContext();
+        System.out.println("[Web Port] : " + applicationContext.getWebServer().getPort());
+    }
+}
--- src/main/java/com/zorba/springbootbasic/SpringBootBasicApplication.java
+++ src/main/java/com/zorba/springbootbasic/SpringBootBasicApplication.java
@@ -1,18 +1,18 @@
 package com.zorba.springbootbasic;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+//import org.apache.catalina.Context;
+//import org.apache.catalina.LifecycleException;
+//import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+//import org.springframework.boot.autoconfigure.web.ServerProperties;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
 
 /**
  * ComponentScan 애노테이션으로 빈 읽어들이고 등록한 다음,
@@ -22,37 +22,37 @@ import java.io.PrintWriter;
  * spring.factories에 있는 키값을 통해 빈들을 등록한다.
  * 빈들도 조건에 따라 생성될지 말지 결정됨
  * */
-//@SpringBootApplication
+@SpringBootApplication
 public class SpringBootBasicApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(SpringBootBasicApplication.class, args);
-//    }
-
-    public static void main(String[] args) throws LifecycleException {
-        Tomcat tomcat = new Tomcat();
-
-        Context context = tomcat.addContext("", "/");
-
-        HttpServlet servlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-                PrintWriter writer = resp.getWriter();
-                writer.println("<html><head><title>");
-                writer.println("Hey, Tomcat!");
-                writer.println("</title></head>");
-                writer.println("<body><h1>Hello Tomcat!</h1></body>");
-                writer.println("</html>");
-            }
-        };
-
-        String servletName = "helloServlet";
-        tomcat.addServlet("", servletName, servlet);
-        context.addServletMappingDecoded("/hi", servletName);
-
-        tomcat.start();
-        tomcat.getServer().await();
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasicApplication.class, args);
     }
+
+//    public static void main(String[] args) throws LifecycleException {
+//        Tomcat tomcat = new Tomcat();
+//
+//        Context context = tomcat.addContext("", "/");
+//
+//        HttpServlet servlet = new HttpServlet() {
+//            @Override
+//            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//                PrintWriter writer = resp.getWriter();
+//                writer.println("<html><head><title>");
+//                writer.println("Hey, Tomcat!");
+//                writer.println("</title></head>");
+//                writer.println("<body><h1>Hello Tomcat!</h1></body>");
+//                writer.println("</html>");
+//            }
+//        };
+//
+//        String servletName = "helloServlet";
+//        tomcat.addServlet("", servletName, servlet);
+//        context.addServletMappingDecoded("/hi", servletName);
+//
+//        tomcat.start();
+//        tomcat.getServer().await();
+//
+//    }
 
 }
