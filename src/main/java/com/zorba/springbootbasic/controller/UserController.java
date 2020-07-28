6d48b6b8dc34ebbc0d0e30a73e04465169bb0586
greekZorba <smiling.jinhak@gmail.com>
2020-07-28 10:07:56 +0900

    json converter test

--- src/main/java/com/zorba/springbootbasic/controller/UserController.java
+++ src/main/java/com/zorba/springbootbasic/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.zorba.springbootbasic.controller;
 
+import com.zorba.springbootbasic.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class UserController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        return user;
     }
 }
